837f1c95584b0d057fd73a7b418aaa70fbcef2e5
TrabelsiAchraf <trabelsiachraf.mobile@gmail.com>
2021-05-12 02:31:48 +0200

    update framework

---
+++ HaplarUI.xcframework/ios-arm64/HaplarUI.framework/Headers/HLButton.swift
@@ -0,0 +1,18 @@
+//
+//  HLButton.swift
+//  HaplarUI
+//
+//  Created by Achraf Trabelsi on 12/05/2021.
+//
+
+import Foundation
+
+public class HLButton {
+    public init() {
+        
+    }
+    
+    public func sayHello() {
+        debugPrint("Hello Bro ..")
+    }
+}

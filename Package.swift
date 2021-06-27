86a061190827c4f4eaef3cfe90a04847a58cce4c
TrabelsiAchraf <trabelsiachraf.mobile@gmail.com>
2021-06-28 00:56:43 +0200

    update pakckage

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HaplarUI",
-            url: "https://github.com/TrabelsiAchraf/UIHaplarFramework/tree/master/Output/HaplarUI.xcframework"
+            url: "https://github.com/TrabelsiAchraf/UIHaplarFramework/tree/master/Output/HaplarUI.zip",
+            checksum: "df5c44269af17a48e07429b5e0235c14aeec7b1684287fae2169972d1306023b"
         ),
     ]
 )
